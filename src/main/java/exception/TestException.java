eeae7396644ea830a2f77de38cc126e9d20db5f4
chenshan <chenshan@qq.com>
2020-11-10 14:15:50 +0800

    exception

---
+++ src/main/java/exception/TestException.java
@@ -0,0 +1,21 @@
+package exception;
+
+import org.junit.Test;
+
+import java.text.MessageFormat;
+
+public class TestSomething {
+
+    @Test
+    public void test1(){
+        String message = "读入你要搜索的内容";
+        boolean f = false;
+        try {
+            MessageFormat format = new MessageFormat(message);
+            f = format.getFormats().length>0;
+        }catch (IllegalArgumentException e){
+            System.out.println("错误的描述格式非法");
+        }
+
+    }
+}
---
+++ src/main/java/exception/impl/BBIErrorCode.java
@@ -0,0 +1,2 @@
+package exception.impl;public class BBIErrorCode {
+}
